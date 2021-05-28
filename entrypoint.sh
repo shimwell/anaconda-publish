c95c86d11d8a32317fa8b61b7614fad225b76d61
Edoardo Pasca <edo.paskino@gmail.com>
2021-05-28 17:40:01 +0100

    allow to test only a version of python/numpy (#5)
    * allow to test only a version of python/numpy
    
    * builds and tests one package

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,16 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
+    eval conda build "-c "${INPUT_CHANNELS} --output-folder . . --no-test
+    if [ ${INPUT_CONVERT_OSX} = true ]; then
+        conda convert -p osx-64 linux-64/*.tar.bz2
+    fi
+    if [ ${INPUT_CONVERT_WIN} = true ]; then
+        conda convert -p win-64 linux-64/*.tar.bz2
+    fi
+}
+
+build_and_test_package(){
     eval conda build "-c "${INPUT_CHANNELS} --output-folder . .
     if [ ${INPUT_CONVERT_OSX} = true ]; then
         conda convert -p osx-64 linux-64/*.tar.bz2
@@ -24,6 +34,12 @@ build_package(){
     if [ ${INPUT_CONVERT_WIN} = true ]; then
         conda convert -p win-64 linux-64/*.tar.bz2
     fi
+}
+
+test_package(){
+    # builds and tests one package
+    eval conda build "-c "${INPUT_CHANNELS} "--python="${INPUT_TEST_PYVER} "--numpy="${INPUT_TEST_NPVER} .
+
 }
 
 upload_package(){
@@ -39,7 +55,13 @@ upload_package(){
 
 go_to_build_dir
 check_if_meta_yaml_file_exists
-build_package
+if  [ ${INPUT_TEST_ALL} = true ]; then
+    build_and_test_package
+else
+    build_package
+    test_package
+fi
+
 # upload package if INPUT_PUBLISH is set to true
 if [ ${INPUT_PUBLISH} = true ]; then
     upload_package
